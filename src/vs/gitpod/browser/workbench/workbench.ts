44ba87e689978da248d3b52939cb2928c5edaed1
Shulammite-Aso <asoamarachi@gmail.com>
2021-11-09 05:18:07 -0800

    switch workbench preferred color themes

--- src/vs/gitpod/browser/workbench/workbench.ts
+++ src/vs/gitpod/browser/workbench/workbench.ts
@@ -831,6 +831,8 @@ async function doStart(): Promise<IDisposable> {
 		},
 		configurationDefaults: {
 			'workbench.colorTheme': 'Gitpod Light',
+			'workbench.preferredLightColorTheme': 'Gitpod Light',
+			'workbench.preferredDarkColorTheme': 'Gitpod Dark',
 		},
 		developmentOptions: {
 			logLevel: logLevel ? parseLogLevel(logLevel) : undefined
